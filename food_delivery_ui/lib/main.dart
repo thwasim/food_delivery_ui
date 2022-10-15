42b8fe7fa4dee203cff77ebaa19220476a04b404
thwasim <fasilfasi9160@gmail.com>
2022-10-15 14:21:56 +0530

    animation commit

---
+++ food_delivery_ui/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/pages/starterpage.dart';
+
+void main() => runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(fontFamily: 'Roboto'),
+    home: StarterPage(),
+  )
+);
